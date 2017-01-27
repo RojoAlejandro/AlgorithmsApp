46c27e832e0e996f73de5684ccedbf176f1e82cc
RojoAlejandro <arojod1100@alumno.ipn.mx>
2017-01-26 23:10:28 -0600

    First Commit

---
+++ app/src/main/java/com/example/alejandro/algorithms/BasicFragment.java
@@ -0,0 +1,43 @@
+package com.example.alejandro.algorithms;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+public class BasicFragment extends Fragment {
+
+    public static final String ARG_PAGE = "ARG:PAGE";
+
+    private int mPage;
+
+    public static BasicFragment newInstance(int page){
+        Bundle args = new Bundle();
+        args.putInt(ARG_PAGE, page);
+        BasicFragment fragment = new BasicFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mPage = getArguments().getInt(ARG_PAGE);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_basic, container, false);
+        TextView textView = (TextView) view;
+        textView.setText("Fragment #" + mPage);
+        return view;
+    }
+
+
+}
