05337cf7865c09c9e70c76290230d4b9c835f19e
Parra <parracarlos17@gmail.com>
2017-02-06 12:44:33 -0600

    Parra: Recycler, CardView, Adapters

---
+++ app/src/main/java/com/example/alejandro/algorithms/Lists/BasicList.java
@@ -0,0 +1,36 @@
+package com.example.alejandro.algorithms.Lists;
+
+/**
+ * Created by Administrador on 30/01/17.
+ */
+
+public class BasicList {
+
+    private String title;
+    private String description;
+    private int imagen;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getImagen() {
+        return imagen;
+    }
+
+    public void setImagen(int imagen) {
+        this.imagen = imagen;
+    }
+}
